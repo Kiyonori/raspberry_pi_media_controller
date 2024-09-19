6ddc914d75e0e0486997fcf18476db2f95bcb870
Kiyonori SAKAI <neco.am@gmail.com>
2024-09-19 11:43:41 +0900

    [Update] Static メソッドに変更した

--- app/analog_digital_converter/GetChannel.py
+++ app/analog_digital_converter/GetChannel.py
@@ -4,22 +4,24 @@ from adafruit_ads1x15.ads1x15 import Mode
 from adafruit_ads1x15.analog_in import AnalogIn
 
 
+def execute(
+        ads: adafruit_ads1x15.ads1115,
+        analog_input_pin_1: int,
+        analog_input_pin_2: int,
+        conversion_mode: int = Mode.CONTINUOUS,
+        samples_per_second: int = 860,
+) -> adafruit_ads1x15.analog_in.AnalogIn:
+    ads.mode = conversion_mode
+    ads.data_rate = samples_per_second
+
+    channel = AnalogIn(
+        ads,
+        analog_input_pin_1,
+        analog_input_pin_2,
+    )
+
+    return channel
+
+
 class GetChannel:
-    def execute(
-            self,
-            ads: adafruit_ads1x15.ads1115,
-            analog_input_pin_1: int,
-            analog_input_pin_2: int,
-            conversion_mode: int = Mode.CONTINUOUS,
-            samples_per_second: int = 860,
-    ) -> adafruit_ads1x15.analog_in.AnalogIn:
-        ads.mode = conversion_mode
-        ads.data_rate = samples_per_second
-
-        channel = AnalogIn(
-            ads,
-            analog_input_pin_1,
-            analog_input_pin_2,
-        )
-
-        return channel
+    pass

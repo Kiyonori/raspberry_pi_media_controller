0562567259227df7b1de07faca761ed1976316da
Kiyonori SAKAI <neco.am@gmail.com>
2024-09-20 12:57:07 +0900

    [Update] メインになるスクリプトから不必要な引数を取り除いた

--- app/myapp.py
+++ app/myapp.py
@@ -1,7 +1,6 @@
 import analog_digital_converter.GetWattage as GetWattage
 import analog_digital_converter.GetADS as GetADS
 import adafruit_ads1x15.ads1115 as ads1115
-from adafruit_ads1x15.ads1x15 import Mode
 
 ads = GetADS.execute()
 
@@ -10,8 +9,6 @@ while True:
         ads,
         ads1115.P0,
         ads1115.P1,
-        Mode.CONTINUOUS,
-        samples_per_second=860,
     )
 
     print(wattage)

c835d3baff56ad6e150d178ac21ba4b5c479a4a6
Kiyonori SAKAI <neco.am@gmail.com>
2024-09-20 12:56:10 +0900

    [Add] チャンネル1または、チャンネル2の消費電力を測定する、測定専用のスクリプトを書いた

---
+++ app/clump_ammeter_channel.py
@@ -0,0 +1,53 @@
+import adafruit_ads1x15.ads1115 as ads1115
+import analog_digital_converter.GetADS as GetADS
+import analog_digital_converter.GetWattage as GetWattage
+import sys
+from datetime import datetime
+
+
+def main() -> None:
+    """
+    チャンネル1または、チャンネル2の消費電力を測定する
+    """
+
+    if sys.argv.__len__() != 2:
+        exit(1)
+
+    channel: str = sys.argv[1]
+    analog_pin_1: int
+    analog_pin_2: int
+
+    match channel:
+        case '1':
+            analog_pin_1 = ads1115.P0
+            analog_pin_2 = ads1115.P1
+        case '2':
+            analog_pin_1 = ads1115.P2
+            analog_pin_2 = ads1115.P3
+        case _:
+            exit(1)
+
+    ads = GetADS.execute()
+
+    try:
+        while True:
+            wattage: float = GetWattage.execute(
+                ads,
+                analog_pin_1,
+                analog_pin_2,
+            )
+
+            print(
+                'Channel: %s, Wattage: %.2f, Timestamp: %s'
+                % (
+                    channel,
+                    wattage,
+                    datetime.now().strftime('%Y-%m-%d %H:%M:%S'),
+                ),
+            )
+
+    except KeyboardInterrupt:
+        exit(0)
+
+
+main()

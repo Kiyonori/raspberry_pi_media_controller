f410ef61f7d6426252c98da06f0b048f248dfdd4
Kiyonori SAKAI <neco.am@gmail.com>
2024-09-20 11:18:10 +0900

    [Fix] ADSインスタンス作成時に、必要なパラメーターを設定した

--- app/analog_digital_converter/GetADS.py
+++ app/analog_digital_converter/GetADS.py
@@ -1,11 +1,17 @@
 import adafruit_ads1x15.ads1115
 import board
 import busio
+from adafruit_ads1x15.ads1x15 import Mode
 
 
-def execute() -> adafruit_ads1x15.ads1115.ADS1115:
+def execute(
+        conversion_mode: int = Mode.CONTINUOUS,
+        samples_per_second: int = 860,
+) -> adafruit_ads1x15.ads1115.ADS1115:
     i2c = busio.I2C(board.SCL, board.SDA)
     ads = adafruit_ads1x15.ads1115.ADS1115(i2c)
+    ads.mode = conversion_mode
+    ads.data_rate = samples_per_second
 
     return ads
 

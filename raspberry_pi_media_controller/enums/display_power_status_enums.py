7c994fbbdc2a1913d6279472bf7207963e586982
Kiyonori SAKAI <neco.am@gmail.com>
2024-10-02 08:49:48 +0900

    #44 [Update] ディスプレイの電源状態を示す Enum を改善。より明確な名称に変更した

--- raspberry_pi_media_controller/enums/display_power_status_enums.py
+++ raspberry_pi_media_controller/enums/display_power_status_enums.py
@@ -15,7 +15,7 @@ class DisplayPowerStatusEnum(Enum):
     POWERED_OFF_AND_SIGNAL_CAN_NOT_BE_RECEIVED = 'POWERED_OFF_AND_SIGNAL_CAN_NOT_BE_RECEIVED'
     """ディスプレイの電源がOFFであり、赤外線信号の受信は出来ません"""
 
-    DISCONNECTED = 'DISCONNECTED'
+    UNPLUGGED = 'UNPLUGGED'
     """電源に接続されていません"""
 
     GLITCHED = 'GLITCHED'

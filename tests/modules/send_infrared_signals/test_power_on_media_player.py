8f65ad51e54c2d6fffcbfd1ac9448b6bf009a280
Kiyonori SAKAI <neco.am@gmail.com>
2024-10-09 09:24:37 +0900

    #8 [Update] test_power_on_media_player_メソッドは_メディアプレイヤーに電源が供給されていない場合は_内部からtoggle_media_player_power_statusメソッドは1度も呼ばれないこと

--- tests/modules/send_infrared_signals/test_power_on_media_player.py
+++ tests/modules/send_infrared_signals/test_power_on_media_player.py
@@ -15,3 +15,19 @@ def test_power_on_media_player_メソッドは_メディアプレイヤーが稼
 ):
     power_on_media_player()
     mock_toggle_media_player_power_status.assert_called_once()
+
+
+@patch(
+    target='raspberry_pi_media_controller.modules.send_infrared_signals.power_on_media_player.toggle_media_player_power_status',
+)
+@patch(
+    target='raspberry_pi_media_controller.modules.send_infrared_signals.power_on_media_player.get_wattages_on_media_player',
+    return_value=[0.4, 0.3, 0.2, 0.4],
+)
+def test_power_on_media_player_メソッドは_メディアプレイヤーに電源が供給されていない場合は_内部からtoggle_media_player_power_statusメソッドは1度も呼ばれないこと(
+        mock_get_wattages_on_media_player,
+        mock_toggle_media_player_power_status,
+
+):
+    power_on_media_player()
+    mock_toggle_media_player_power_status.assert_not_called()

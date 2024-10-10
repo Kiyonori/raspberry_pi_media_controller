a3bb4ca099b0b484c75338324e71bd58af74d972
Kiyonori SAKAI <neco.am@gmail.com>
2024-10-10 13:58:19 +0900

    #63 [Update] メディアプレイヤーの電源操作の再試行に関するメンバ変数をData Transfer Objectに記述した

--- raspberry_pi_media_controller/data/media_player_power_config_data.py
+++ raspberry_pi_media_controller/data/media_player_power_config_data.py
@@ -15,14 +15,24 @@ class MediaPlayerPowerConfigData:
     trouble_wattage: float
     """消費電力がこの値以上の場合、メディアプレイヤーが故障しています"""
 
+    handling_maximum_number_of_attempts: int
+    """電源操作の最大試行回数"""
+
+    handling_waiting_seconds: int
+    """電源操作の再試行までの待ち秒数"""
+
     def __init__(
             self,
             unplugged_wattage: float,
             powered_off_and_signal_can_be_received_wattages: list[float],
             powered_on_wattages: list[float],
             trouble_wattage: float,
+            handling_maximum_number_of_attempts: int,
+            handling_waiting_seconds: int,
     ):
         self.unplugged_wattage = unplugged_wattage
         self.powered_off_and_signal_can_be_received_wattages = powered_off_and_signal_can_be_received_wattages
         self.powered_on_wattages = powered_on_wattages
         self.trouble_wattage = trouble_wattage
+        self.handling_maximum_number_of_attempts = handling_maximum_number_of_attempts
+        self.handling_waiting_seconds = handling_waiting_seconds

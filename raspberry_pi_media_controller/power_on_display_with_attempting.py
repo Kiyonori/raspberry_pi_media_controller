83c4bca5d51888ef0a698a5c637573345a75be19
Kiyonori SAKAI <neco.am@gmail.com>
2024-10-11 09:49:23 +0900

    #16 [Update] ディスプレイの電源を付ける際、再試行処理を取りやめる条件を、より適切にした
    これらに一致する場合は、再試行せずに取りやめます
    
    - ディスプレイの電源がONです
    - 電源に接続されていません
    - 機器の故障

--- raspberry_pi_media_controller/power_on_display_with_attempting.py
+++ raspberry_pi_media_controller/power_on_display_with_attempting.py
@@ -2,8 +2,8 @@ from raspberry_pi_media_controller.data.display_power_config_data \
     import DisplayPowerConfigData
 from raspberry_pi_media_controller.enums.display_power_status_enums \
     import DisplayPowerStatusEnum
-from raspberry_pi_media_controller.modules.analog_digital_converter.get_wattages_on_display import \
-    get_wattages_on_display
+from raspberry_pi_media_controller.modules.analog_digital_converter.get_wattages_on_display \
+    import get_wattages_on_display
 from raspberry_pi_media_controller.modules.config.get_display_power_config \
     import get_display_power_config
 from raspberry_pi_media_controller.modules.get_power_statuses.get_display_power_status \
@@ -20,20 +20,25 @@ def main():
 
     attempt_count: int = 0
     config: DisplayPowerConfigData = get_display_power_config()
+    power_status: DisplayPowerStatusEnum
 
     while True:
         wattages: list[float] = get_wattages_on_display()
-        power_status: DisplayPowerStatusEnum = get_display_power_status(wattages)
+        power_status = get_display_power_status(wattages)
 
-        if power_status is DisplayPowerStatusEnum.POWERED_ON:
-            break
+        match power_status:
+            case (
+                DisplayPowerStatusEnum.POWERED_ON,
+                DisplayPowerStatusEnum.UNPLUGGED,
+                DisplayPowerStatusEnum.TROUBLE
+            ):
+                break
 
         if attempt_count > config.handling_maximum_number_of_attempts:
             break
 
         toggle_display_power_status()
         sleep(config.handling_waiting_seconds)
-
         attempt_count += 1
 
 

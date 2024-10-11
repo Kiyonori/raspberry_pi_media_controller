7dd17231ade9d3558d5659be49aca402122d85a4
Kiyonori SAKAI <neco.am@gmail.com>
2024-10-11 13:31:59 +0900

    #16 [Update] get_wattages_on_display をモック化して、数秒間の測定なしに、即時に固定のワット数を返すようにして、テストを高速化

--- tests/test_power_on_display_with_attempting.py
+++ tests/test_power_on_display_with_attempting.py
@@ -4,31 +4,53 @@ from unittest.mock import patch
 
 
 def test_power_on_display_with_attempting_はディスプレイの電源状態が_POWERED_OFF_AND_SIGNAL_CAN_BE_RECEIVED_であるとき_内部からtoggle_display_power_statusメソッドを呼び_電源投入を試みること():
+    # get_wattages_on_display をモック化して
+    # 数秒間の測定なしに、即時に固定のワット数を返すようにして、テストを高速化
     with patch(
-            'raspberry_pi_media_controller.power_on_display_with_attempting.toggle_display_power_status'
-    ) as mock_toggle_display_power_status:
-        with patch(
-                'raspberry_pi_media_controller.power_on_display_with_attempting.get_display_power_status'
-        ) as mock_get_display_power_status:
-            mock_get_display_power_status.side_effect = [
-                DisplayPowerStatusEnum.POWERED_OFF_AND_SIGNAL_CAN_BE_RECEIVED,
-                DisplayPowerStatusEnum.POWERED_ON,
-            ]
+            'raspberry_pi_media_controller.power_on_display_with_attempting.get_wattages_on_display'
+    ) as mock_get_wattages_on_display:
+        mock_get_wattages_on_display.return_value = [30, 30, 30, 30]
 
-            main()
-            mock_toggle_display_power_status.assert_called_once()
+        # toggle_display_power_status をモック化して
+        # 呼び出し回数をアサート
+        with patch(
+                'raspberry_pi_media_controller.power_on_display_with_attempting.toggle_display_power_status'
+        ) as mock_toggle_display_power_status:
+            # get_display_power_status をモック化して、
+            # 呼び出し毎にテストの意図に合う値を返す
+            with patch(
+                    'raspberry_pi_media_controller.power_on_display_with_attempting.get_display_power_status'
+            ) as mock_get_display_power_status:
+                mock_get_display_power_status.side_effect = [
+                    DisplayPowerStatusEnum.POWERED_OFF_AND_SIGNAL_CAN_BE_RECEIVED,
+                    DisplayPowerStatusEnum.POWERED_ON,
+                ]
+
+                main()
+                mock_toggle_display_power_status.assert_called_once()
 
 
 def test_power_on_display_with_attempting_はディスプレイの電源状態が_すでに_POWERED_ON_であるとき_内部からtoggle_display_power_statusメソッドが一度も呼ばれないこと():
+    # get_wattages_on_display をモック化して
+    # 数秒間の測定なしに、即時に固定のワット数を返すようにして、テストを高速化
     with patch(
-            'raspberry_pi_media_controller.power_on_display_with_attempting.toggle_display_power_status'
-    ) as mock_toggle_display_power_status:
-        with patch(
-                'raspberry_pi_media_controller.power_on_display_with_attempting.get_display_power_status'
-        ) as mock_get_display_power_status:
-            mock_get_display_power_status.side_effect = [
-                DisplayPowerStatusEnum.POWERED_ON,
-            ]
+            'raspberry_pi_media_controller.power_on_display_with_attempting.get_wattages_on_display'
+    ) as mock_get_wattages_on_display:
+        mock_get_wattages_on_display.return_value = [30, 30, 30, 30]
 
-            main()
-            mock_toggle_display_power_status.assert_not_called()
+        # toggle_display_power_status をモック化して
+        # 呼び出し回数をアサート
+        with patch(
+                'raspberry_pi_media_controller.power_on_display_with_attempting.toggle_display_power_status'
+        ) as mock_toggle_display_power_status:
+            # get_display_power_status をモック化して、
+            # 呼び出し毎にテストの意図に合う値を返す
+            with patch(
+                    'raspberry_pi_media_controller.power_on_display_with_attempting.get_display_power_status'
+            ) as mock_get_display_power_status:
+                mock_get_display_power_status.side_effect = [
+                    DisplayPowerStatusEnum.POWERED_ON,
+                ]
+
+                main()
+                mock_toggle_display_power_status.assert_not_called()

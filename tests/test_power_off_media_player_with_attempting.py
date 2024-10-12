338aa2657b560f96a8eea815b1f1c7778eda7b83
Kiyonori SAKAI <neco.am@gmail.com>
2024-10-12 13:48:12 +0900

    #19 [Update] test_power_off_media_player_with_attempting_はメディアプレイヤーの電源状態が_POWERED_ON_である場合_内部からtoggle_media_player_power_statusメソッドを呼んで終了を試みる最大回数は10回であること

--- tests/test_power_off_media_player_with_attempting.py
+++ tests/test_power_off_media_player_with_attempting.py
@@ -132,3 +132,28 @@ def test_power_off_media_player_with_attempting_はメディアプレイヤー�
                 ]
                 main()
                 assert mock_toggle_media_player_power_status.call_count == 3
+
+
+def test_power_off_media_player_with_attempting_はメディアプレイヤーの電源状態が_POWERED_ON_である場合_内部からtoggle_media_player_power_statusメソッドを呼んで終了を試みる最大回数は10回であること():
+    # get_wattages_on_media_player をモック化して
+    # 数秒間の測定なしに、即時に固定のワット数を返すようにして、テストを高速化
+    with patch(
+            'raspberry_pi_media_controller.power_off_media_player_with_attempting.get_wattages_on_media_player'
+    ) as mock_get_wattages_on_media_player:
+        mock_get_wattages_on_media_player.return_value = [30, 30, 30, 30]
+
+        # toggle_media_player_power_status をモック化して
+        # 呼び出し回数をアサート
+        with patch(
+                'raspberry_pi_media_controller.power_off_media_player_with_attempting.toggle_media_player_power_status'
+        ) as mock_toggle_media_player_power_status:
+            # get_media_player_power_status をモック化して、
+            # 呼び出し毎にテストの意図に合う値を返す
+            with patch(
+                    'raspberry_pi_media_controller.power_off_media_player_with_attempting.get_media_player_power_status'
+            ) as mock_get_media_player_power_status:
+                # ℹ️
+                mock_get_media_player_power_status.return_value = MediaPlayerPowerStatusEnum.POWERED_ON
+
+                main()
+                assert mock_toggle_media_player_power_status.call_count == 10

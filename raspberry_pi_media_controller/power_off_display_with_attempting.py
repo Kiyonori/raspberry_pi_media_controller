03da670a745412eba29a9804583294f5275606ca
Kiyonori SAKAI <neco.am@gmail.com>
2024-10-16 10:36:27 +0900

    #47 02 [Update] ディスプレイの電源OFF時にログを出力する

--- raspberry_pi_media_controller/power_off_display_with_attempting.py
+++ raspberry_pi_media_controller/power_off_display_with_attempting.py
@@ -1,3 +1,4 @@
+import logging
 from raspberry_pi_media_controller.data.display_power_config_data \
     import DisplayPowerConfigData
 from raspberry_pi_media_controller.enums.display_power_status_enums \
@@ -8,6 +9,14 @@ from raspberry_pi_media_controller.modules.config.get_display_power_config \
     import get_display_power_config
 from raspberry_pi_media_controller.modules.get_power_statuses.get_display_power_status \
     import get_display_power_status
+from raspberry_pi_media_controller.modules.my_logger.flatten_wattages \
+    import flatten_wattages
+from raspberry_pi_media_controller.modules.my_logger.get_last_message_on_power_off_display \
+    import get_last_message_on_power_off_display
+from raspberry_pi_media_controller.modules.my_logger.initialize_logger \
+    import initialize_logger
+from raspberry_pi_media_controller.modules.my_logger.make_log_directory \
+    import make_log_directory
 from raspberry_pi_media_controller.modules.send_infrared_signals.toggle_display_power_status \
     import toggle_display_power_status
 from time import sleep
@@ -21,6 +30,9 @@ def main():
     attempt_count: int = 0
     config: DisplayPowerConfigData = get_display_power_config()
     power_status: DisplayPowerStatusEnum
+    log_dir: str = make_log_directory()
+    initialize_logger(log_dir)
+    logging.info('The system will turn off the display.')
 
     while True:
         wattages: list[float] = get_wattages_on_display()
@@ -36,9 +48,28 @@ def main():
         if attempt_count > config.handling_maximum_number_of_attempts - 1:
             break
 
+        logging.info(
+            'Attempt %d times: Turn off the power to the display. Wattages: %s' %
+            (
+                int(attempt_count + 1),
+                flatten_wattages(wattages),
+            )
+        )
+
         toggle_display_power_status()
         sleep(config.handling_waiting_seconds)
         attempt_count += 1
+
+    last_message: str = get_last_message_on_power_off_display(
+        attempt_count,
+        power_status,
+    )
+
+    logging.info(
+        last_message + \
+        ' Wattages: ' + \
+        flatten_wattages(wattages)
+    )
 
 
 if __name__ == "__main__":

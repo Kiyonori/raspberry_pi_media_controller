5bb0c23253ce64e3f19e025ce74f4a1502bb2130
Kiyonori SAKAI <neco.am@gmail.com>
2024-10-16 10:25:12 +0900

    #47 02 [Add] test_get_last_message_on_power_off_display_は試行回数に関係なく_電源が接続されていない場合のメッセージは想定どおりであること

---
+++ tests/modules/my_logger/test_get_last_message_on_power_off_display.py
@@ -0,0 +1,26 @@
+import pytest
+from raspberry_pi_media_controller.enums.display_power_status_enums \
+    import DisplayPowerStatusEnum
+from raspberry_pi_media_controller.modules.my_logger.get_last_message_on_power_off_display \
+    import get_last_message_on_power_off_display
+
+
+@pytest.mark.parametrize(
+    "attempt_count, power_status",
+    [
+        (0, DisplayPowerStatusEnum.UNPLUGGED),
+        (1, DisplayPowerStatusEnum.UNPLUGGED),
+        (9, DisplayPowerStatusEnum.UNPLUGGED),
+        (10, DisplayPowerStatusEnum.UNPLUGGED),
+    ],
+)
+def test_get_last_message_on_power_off_display_は試行回数に関係なく_電源が接続されていない場合のメッセージは想定どおりであること(
+        attempt_count: int,
+        power_status: DisplayPowerStatusEnum,
+):
+    message: str = get_last_message_on_power_off_display(
+        attempt_count,
+        power_status,
+    )
+
+    assert message == 'The display power plug is unplugged.'
